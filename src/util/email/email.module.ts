a4ee944e76f90b0d190c556d58f263cdf33f08e9
จิรภัทร แซ่จึง <skull.death1994@gmail.com>
2026-04-20 12:39:04 +0700

    clean up be for email

--- src/util/email/email.module.ts
+++ src/util/email/email.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailService } from './email.service';
-import { EmailTemplatesService } from './templates/email-templates.service';
-import { EmailNotificationService } from './email-notification.service';
-import { EmailController } from './email.controller';
-import { User } from 'src/users/entities/user.entity';
-import { WorkHistory } from 'src/work-history/entities/work-history.entity';
-import { DevelopmentPlan } from 'src/development-plan/entities/development-plan.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, WorkHistory, DevelopmentPlan])],
-  providers: [EmailService, EmailTemplatesService, EmailNotificationService],
-  controllers: [EmailController],
-  exports: [EmailService, EmailTemplatesService, EmailNotificationService],
+  providers: [EmailService],
+  exports: [EmailService],
 })
 export class EmailModule {}
--- src/util/email/index.ts
+++ src/util/email/index.ts
@@ -3,6 +3,3 @@ export * from './providers/gmail.provider';
 export * from './providers/postmark.provider';
 export * from './email.service';
 export * from './email.module';
-export * from './templates/email-templates.service';
-export * from './email-notification.service';
-export * from './email.controller';
